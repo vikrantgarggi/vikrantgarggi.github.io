3d25589ff51345e8e7136f75c2be12da1c8c64bf
Vikrant <vkrntgrg.app@gmail.com>
2026-01-30 13:46:25 +0530

    C

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: "/", // required for user GitHub Pages
+  base: "/vikrantgarggi/", // required for user GitHub Pages
   plugins: [react()],
   resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
 });
