df1da0b755c879ae3012fdc9902fd160cd7a4aab
vikrantgarggi <vikrantgarg.gi@gmail.com>
2026-01-30 11:52:44 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   // Set base for GitHub Pages deployment
-  base: 'https://vikrantgarggi.github.io/',
+  base: '/',
   server: {
     host: "::",
     port: 8080,
